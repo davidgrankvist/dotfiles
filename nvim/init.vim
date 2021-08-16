2d4385dfb4a2704f88806247b4d082ff74cce853
David Grankvist <grankvist.david@gmail.com>
2021-08-16 21:17:06 +0200

    Clean up init file formatting

--- nvim/init.vim
+++ nvim/init.vim
@@ -1,11 +1,9 @@
 source $HOME/.config/nvim/vim-plug/plugins.vim
 
 nnoremap <c-p> :Files<cr>
+
 set number relativenumber
-
 set completeopt=menuone,noinsert,noselect
-
-" Avoid showing extra messages when using completion
 set shortmess+=c
 
 let g:netrw_liststyle = 3
--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -1,17 +1,12 @@
--- Configure LSP
--- https://github.com/neovim/nvim-lspconfig#rust_analyzer
-
--- nvim_lsp object
+----------    LSP    ----------
 local nvim_lsp = require'lspconfig'
 
--- function to attach completion when setting up lsp
 local on_attach = function(client)
   require'completion'.on_attach(client)
 end
 
--- Enable rust_analyzer
 nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
--- Enable diagnostics
+
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
         virtual_text = true,
@@ -20,7 +15,7 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     }
 )
 
--- Compe setup
+----------    Auto completion    ----------
 require'compe'.setup {
   enabled = true;
   autocomplete = true;
