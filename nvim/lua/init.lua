50c60b2468e9ddf2490885a2561fbd8bdbe277c5
David Grankvist <grankvist.david@gmail.com>
2021-08-30 20:07:30 +0200

    Add lsp conf for js/ts

--- nvim/lua/init.lua
+++ nvim/lua/init.lua
@@ -3,6 +3,7 @@ local nvim_lsp = require'lspconfig'
 
 nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
 nvim_lsp.pyright.setup({})
+nvim_lsp.tsserver.setup({})
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
     vim.lsp.diagnostic.on_publish_diagnostics, {
