dbcb31b9f3ba8266816018001fdd087e866f34f8
David Grankvist <grankvist.david@gmail.com>
2021-09-01 14:18:20 +0200

    Separate lsp and autocompletion config

---
+++ nvim/lua/lsp.lua
@@ -0,0 +1,13 @@
+local nvim_lsp = require'lspconfig'
+
+nvim_lsp.rust_analyzer.setup({ on_attach=on_attach })
+nvim_lsp.pyright.setup({})
+nvim_lsp.tsserver.setup({})
+
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = true,
+        signs = true,
+        update_in_insert = true,
+    }
+)
