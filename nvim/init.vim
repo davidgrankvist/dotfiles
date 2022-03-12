c2250f972c2d19c127e343209e7bcd1f3a48f931
David Grankvist <grankvist.david@gmail.com>
2022-03-12 12:16:08 +0100

    Move cycle diagnostics keybindings to lsp.lua

--- nvim/init.vim
+++ nvim/init.vim
@@ -2,9 +2,6 @@ source $HOME/.config/nvim/vim-plug/plugins.vim
 
 " fzf
 nnoremap <c-p> :GFiles<cr>
-" cycle LSP diagnostics
-nnoremap <silent> H           <cmd>lua vim.lsp.diagnostic.goto_prev()<cr>
-nnoremap <silent> L           <cmd>lua vim.lsp.diagnostic.goto_next()<cr>
 
 set number relativenumber
 set completeopt=menuone,noinsert,noselect
--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -12,8 +12,8 @@ vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
 -- See `:help vim.diagnostic.*` for documentation on any of the below functions
 local opts = { noremap=true, silent=true }
 vim.api.nvim_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-vim.api.nvim_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
-vim.api.nvim_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'H', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+vim.api.nvim_set_keymap('n', 'L', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
 vim.api.nvim_set_keymap('n', '<space>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', opts)
 
 -- Use an on_attach function to only map the following keys
