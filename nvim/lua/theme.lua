97ae4cd4ceb269321d563e3268f666cc2a02d3ce
David Grankvist <grankvist.david@gmail.com>
2022-04-24 18:03:37 +0200

    nvim theme: use single quotes

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,12 +1,12 @@
 require('github-theme').setup({
-    theme_style = "dark",
+    theme_style = 'dark',
     -- no italics
-    comment_style = "NONE",
-    keyword_style = "NONE",
-    function_style = "NONE",
-    variable_style = "NONE",
+    comment_style = 'NONE',
+    keyword_style = 'NONE',
+    function_style = 'NONE',
+    variable_style = 'NONE',
     -- increased contrast for e.g. lsp hover
     colors = {
-        bg_float = "#000000",
+        bg_float = '#000000',
     }
 })
