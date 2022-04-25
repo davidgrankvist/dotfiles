05b97987b3ce66ecfa2854baf1163a2db5bc86e9
David Grankvist <grankvist.david@gmail.com>
2022-04-25 22:41:31 +0200

    nvim: theme override for hover and statusline

--- nvim/lua/theme.lua
+++ nvim/lua/theme.lua
@@ -1,3 +1,4 @@
+local util = require('github-theme.util')
 require('github-theme').setup({
     theme_style = 'dark',
     -- no italics
@@ -5,8 +6,13 @@ require('github-theme').setup({
     keyword_style = 'NONE',
     function_style = 'NONE',
     variable_style = 'NONE',
-    -- increased contrast for e.g. lsp hover
-    colors = {
-        bg_float = '#000000',
-    }
+
+    overrides = function(c)
+        return {
+            -- increased contrast for e.g. lsp hover
+            NormalFloat = { fg = c.fg, bg = '#141414' },
+            -- make horizontal splits easier to see
+            StatusLineNC = { fg = util.darken(c.fg, 0.9), bg = util.lighten(c.bg, 0.9) },
+        }
+    end
 })
