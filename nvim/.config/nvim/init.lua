afa77a2f9832a6eb7d4e243c284e82a688652318
David Grankvist <grankvist.david@gmail.com>
2025-06-03 23:57:18 +0200

    nvim: merge win32/unix configs

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -1,19 +1,15 @@
 if vim.fn.has("win32") == 1 then
     vim.cmd("language en")
-    require("common")
-    require("dotfileCommands")
-    require("searchBuiltin")
-    require("theme")
-else
-    require("common")
-    require("config.lazy")
-    require("dotfileCommands")
-    require("searchTelescope")
-    require("autocompletion")
-    require("lsp")
-    require("theme")
 end
 
 if vim.fn.has("wsl") == 1 then
     require("wslClipboard")
 end
+
+require("common")
+require("dotfileCommands")
+require("config.lazy")
+require("searchTelescope")
+require("autocompletion")
+require("lsp")
+require("theme")
