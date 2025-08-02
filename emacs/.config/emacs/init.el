2fdc2aa4b8ef6096437f3ff3e1ca6825773f302f
David Grankvist <grankvist.david@gmail.com>
2025-08-03 00:07:59 +0200

    emacs: basic org mode bindings and separate custom.el

--- emacs/.config/emacs/init.el
+++ emacs/.config/emacs/init.el
@@ -9,8 +9,7 @@
 (load-theme 'tango-dark)
 (if (eq system-type 'windows-nt)
   (set-face-attribute 'default nil :family "DejaVu Sans Mono" :height 75)
-  (set-face-attribute 'default nil :family "DejaVu Sans Mono")
-)
+  (set-face-attribute 'default nil :family "DejaVu Sans Mono"))
 
 ;; Line and column numbers
 (setq display-line-numbers-type 'relative)
@@ -18,10 +17,18 @@
 (column-number-mode 1)
 
 ;; C mode
-(defun my-c-mode-hook ()
+(defun my/c-mode-hook ()
   ;; Indent = 4 spaces
   (setq c-basic-offset 4)
   (setq indent-tabs-mode nil)
-  (c-set-offset 'case-label '+)
-)
-(add-hook 'c-mode-hook 'my-c-mode-hook)
+  (c-set-offset 'case-label '+))
+(add-hook 'c-mode-hook 'my/c-mode-hook)
+
+;; Org mode
+(global-set-key (kbd "C-c l") #'org-store-link)
+(global-set-key (kbd "C-c a") #'org-agenda)
+(global-set-key (kbd "C-c c") #'org-capture)
+
+;; Store settings that were automatically added by Customize in a separate file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
+(load custom-file)
