8c20441ee90f15141481d53d1a223aada05455a4
David Grankvist <grankvist.david@gmail.com>
2025-08-28 20:13:16 +0200

    windows: add devshell commands
    Add aliases to make it easier to launch a VS dev shell in an existing powershell/cmd session.

--- windows/windows_home/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
+++ windows/windows_home/Documents/WindowsPowershell/Microsoft.PowerShell_profile.ps1
@@ -3,5 +3,10 @@ $Env:Path = "C:\Program Files\Git\usr\bin\;${Env:Path}"
 
 New-Alias -Name "g" git
 New-Alias -Name "v" nvim
+New-Alias -Name "devshell" Run-Dev-Shell
 
 Set-PSReadlineKeyHandler -K ctrl+d -Function ViExit
+
+function Run-Dev-Shell {
+    & "C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\Launch-VsDevShell.ps1"
+}
--- windows/windows_home/Documents/cmd/cmd_profile.bat
+++ windows/windows_home/Documents/cmd/cmd_profile.bat
@@ -6,5 +6,7 @@ REM cmd.exe /K "%USERPROFILE%\Documents\cmd\cmd_profile.bat"
 
 REM Make core utils like find/grep available.
 set PATH=C:\Program Files\Git\usr\bin\;%PATH%
+set RUN_DEV_SHELL=C:\Program Files\Microsoft Visual Studio\2022\Community\Common7\Tools\VsDevCmd.bat
 
 doskey g=git $*
+doskey devshell=call "%RUN_DEV_SHELL%"
